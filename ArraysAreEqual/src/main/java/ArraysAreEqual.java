476cff564edff4ce58693f3fb00605980a2459a1
Joseph Dabreu <joeydabreu@gmail.com>
2023-01-04 16:57:36 +0000

    Saved progress on Contains 2023-01-04 16:57:36 +0000

--- ArraysAreEqual/src/main/java/ArraysAreEqual.java
+++ ArraysAreEqual/src/main/java/ArraysAreEqual.java
@@ -15,6 +15,10 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
+        if(a.equals(b))
+        {
+            return true;
+        }
         return false;
     }
 }
