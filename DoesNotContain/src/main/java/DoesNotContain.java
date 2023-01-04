a640932ac63eab6227fb98ebb8fbb38011bba5e8
Joseph Dabreu <joeydabreu@gmail.com>
2023-01-04 17:30:49 +0000

    Saved progress on Contains 2023-01-04 17:30:49 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -14,11 +14,12 @@ public class DoesNotContain {
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] != target)
+            if(arr[i] == target)
             {
-                return true;
+                return false;
             }
         }
-        return false;
+
+        return true;
     }
 }
