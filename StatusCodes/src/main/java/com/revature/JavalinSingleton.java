26a42a76c8e19e312911dcc0fed8881e3b271867
Joseph Dabreu <joeydabreu@gmail.com>
2023-01-10 17:17:57 +0000

    Saved progress on PathParameters 2023-01-10 17:17:57 +0000

--- StatusCodes/src/main/java/com/revature/JavalinSingleton.java
+++ StatusCodes/src/main/java/com/revature/JavalinSingleton.java
@@ -15,7 +15,7 @@ public class JavalinSingleton {
         app.get("/client-side-err", ctx -> {
         
             //write logic here
-
+            ctx.pathParam("")
         });
 
         return app;
