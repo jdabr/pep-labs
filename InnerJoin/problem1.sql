944550b946c90938be53dce68207bf795fb17123
Joseph Dabreu <joeydabreu@gmail.com>
2023-01-13 13:08:50 +0000

    Saved progress on InnerJoin 2023-01-13 13:08:50 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,3 +1,1 @@
-SELECT * FROM student 
-INNER JOIN class ON class.class_title = student.class_title
---WHERE class.id = 1;
+SELECT * FROM student INNER JOIN class ON student.class_title = class.class_title WHERE class.teacher_name = 'Ms. Lovelace';
