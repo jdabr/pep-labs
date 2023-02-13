910c8fc25b38ddcd11cf522df814b97411ee30a0
Joseph Dabreu <joeydabreu@gmail.com>
2023-02-13 18:20:41 +0000

    Saved progress on SubArray 2023-02-13 18:20:41 +0000

--- SubArray/src/main/java/SubArray.java
+++ SubArray/src/main/java/SubArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class SubArray {
     /**
@@ -13,6 +14,7 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int[] subArray = Arrays.copyOfRange(nums, start, end);
+        return subArray;
     }
 }
